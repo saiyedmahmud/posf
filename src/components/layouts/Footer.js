bbf3abbf7e9eff8cb9034b1039abd37d01abd410
saiyed <saiyedmahmud05@gmail.com>
2024-06-09 10:20:06 +0600

    first commit

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,29 @@
+import { Col, Layout, Row } from "antd";
+import styles from "./Footer.module.css";
+
+function Footer() {
+	const { Footer: AntFooter } = Layout;
+	const year = new Date().getFullYear();
+
+	return (
+		<AntFooter className={styles.footer}>
+			<Row>
+				<Col xs={24} md={24} lg={12} className={styles.copyrightCol}>
+					<p className={styles.copyrightText}>
+						{year}{" "}
+						<a
+							href='https://solution.omega.ac'
+							className='font-weight-bold'
+							target='_blank'
+							rel='noreferrer'>
+							Potato Squad
+						</a>{" "}
+					</p>
+				</Col>
+				<Col xs={24} md={24} lg={12}></Col>
+			</Row>
+		</AntFooter>
+	);
+}
+
+export default Footer;
